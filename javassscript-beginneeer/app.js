e94d86bd73a9f9fe2312be4317f0caf5f5485455
@AlenaYatsynovich <elen7staver@gmail.com>
2021-03-05 00:28:17 +0300

    add file app.js

---
+++ javassscript-beginneeer/app.js
@@ -0,0 +1,84 @@
+// 1 Переменные
+// camelCase
+// const firstName = 'Elen'
+// const age = 50 // number
+// const isProgrammer = true // boolean
+
+// const _ = 'private'
+// const $ = 'some value'
+
+// const if = 'mkef' // err
+// const withNum5 = '5'
+// const 5withNum = 5 // err
+
+// 2 Мутирование
+// console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+let currentYear = 2020
+const birthYear = 1971
+
+// const age = currentYear + birthYear
+
+const a = 10
+const b = 5
+
+let c = 32
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(++currentYear)
+// console.log(--currentYear)
+// console.log(c)
+
+// 4 Типы данных
+// const isProgrammer = true
+// const name = 'Elena'
+// const age = 50
+// let x
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+// console.log(typeof null)
+
+// 5 Приоритет операторов
+
+
+// > < >= <=
+// const isFullAge = (currentYear - birthYear) >= fullAge // 50 >= 51 => true
+// console.log(isFullAge)
+
+// 6 Условные операторы
+const courseStatus = 'pending' // ready. fail. pending
+
+if (courseStatus === 'readi') {
+console.log('Курс уже готов и его можно проходить')
+}else if (courseStatus === 'pending') {
+    console.log('Курс пока находится в процессе разработки')
+    }
+
+   
+
+
+
+
+
+
+
+
+ 
